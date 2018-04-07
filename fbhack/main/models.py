2183d69e7935257905aa27c2881c832b9e22018c
Marcos Camargo <marcoscrcamargo@gmail.com>
2018-04-07 15:48:35 -0300

    Models finalizados (falta testar)

--- fbhack/main/models.py
+++ fbhack/main/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+	tag = models.CharField(max_length=50)
+
 class User(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	is_specialist = models.BooleanField(default=False)
-	skills = models.ManyToMany(Skill, verbose_name='list of skills')
-
-class Skill(models.Model):
-	tag = models.CharField(max_length=50)
-
-class Chat(models.Model):
-	user_instructor = models.ForeignKey(User)
-	user_student = models.ForeignKey(User)
+	skills = models.ManyToManyField(Skill, verbose_name='list of skills')
 
 class Question(models.Model):
 	question = models.TextField()
-	skills = models.ManyToMany(Skill, verbose_name='question skills')
+	skills = models.ManyToManyField(Skill, verbose_name='question skills')
+
+class Chat(models.Model):
+	user_instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='instructor')
+	user_student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student')
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
