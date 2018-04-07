942c7db72df38d22a9407002d7f57e3a719e0df3
Marcos Camargo <marcoscrcamargo@gmail.com>
2018-04-07 17:59:15 -0300

    urls.py atualizado para views.

--- fbhack/fbhack/urls.py
+++ fbhack/fbhack/urls.py
@@ -22,4 +22,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^user/$', views.UserList.as_view()),
     url(r'^user/(?P<pk>[0-9]+)/$', views.UserViewSet.as_view()),
+    url(r'^skill/$', views.SkillList.as_view()),
+    url(r'^skill/(?P<pk>[0-9]+)/$', views.SkillwSet.as_view()),
+    url(r'^question/$', views.QuestionList.as_view()),
+    url(r'^question/(?P<pk>[0-9]+)/$', views.QuestionViewSet.as_view()),
+    url(r'^chat/$', views.ChatList.as_view()),
+    url(r'^chat/(?P<pk>[0-9]+)/$', views.ChatViewSet.as_view()),
 ]
